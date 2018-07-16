81f12047194433f68c3bdb90097aa5804c9fa8d9
汤旗 <tangqihlw@bbktel.com>
2018-07-16 13:52:19 +0800

    init

--- spring-boot-hello/src/main/java/com/vivo/web/HelloController.java
+++ spring-boot-hello/src/main/java/com/vivo/web/HelloController.java
@@ -3,6 +3,11 @@ package com.vivo.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试控制器
+ *
+ * @author Administrator
+ */
 @RestController
 public class HelloController {
 
