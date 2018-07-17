7394e74f37e5a004343243e50af82c8302720dd2
汤旗 <tangqihlw@bbktel.com>
2018-07-17 21:37:21 +0800

    修正

--- spring-boot-hello/src/main/java/com/jdktomcat/web/HelloController.java
+++ spring-boot-hello/src/main/java/com/jdktomcat/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.jdktomcat.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/hello")
-	public String index() {
-		return "Hello World";
-	}
+    @RequestMapping("/hello")
+    public String index(@RequestParam(value = "id", required = true) Integer id) {
+        return "Hello World " + id;
+    }
 
 }
