72cf5f342392ea5ea7d1d23bcd0f53c34f66b814
汤旗 <tangqihlw@bbktel.com>
2018-10-30 14:58:20 +0800

    注释

--- eureka-server/src/main/java/com/jdktomcat/Application.java
+++ eureka-server/src/main/java/com/jdktomcat/Application.java
@@ -4,12 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author jdktomcat
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
 
 }
